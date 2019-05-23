afb1374bba847428120d5dcb848b5a390575259e
Hasan Abdullah <47323055+hasan282@users.noreply.github.com>
2019-05-23 16:58:49 +0700

    Latihan Batik
    Edit Tampilan

---
+++ KirimBatikOOP/src/kirimbatikoop/KirimBatikOOP.java
@@ -0,0 +1,12 @@
+package kirimbatikoop;
+
+public class KirimBatikOOP {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormBatik fbat = new FormBatik();
+        fbat.setVisible(true);
+        fbat.setTitle("Form Input Pengiriman");
+    }
+    
+}
